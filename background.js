20499362403dc44d8be59806e297bf5959726e8d
shintynash <30459387+shintynash@users.noreply.github.com>
2018-06-27 22:46:34 +0100

    Use start of force.com SID to find salesforce.com SID

--- background.js
+++ background.js
@@ -7,19 +7,19 @@ var workbenchtoolsforchrome = {
     badPageErrorMsg : 'Ooops! This command must only be excuted when logged into Salesforce. Select a tab with an active Salesforce session and try again.',
         
     onRequest : function(request, sender, sendResponse) {
-	
+
       // Show the page action for the tab that the sender (content script) was on.
-		if (sender.tab.url.indexOf('blacktaboid') > -1 && sender.tab.url.indexOf('servlet/servlet.su') == -1) {
-			chrome.pageAction.setIcon({'tabId':sender.tab.id, 'path':'workbench-3-black-cube-48x48.png'});
-		}
-	// Check if there is a SID related to the page the content script ran on
-		chrome.cookies.getAll({"url":sender.tab.url,"name":"sid"},function (cookie){
-			console.log (cookie.length);
-			console.log(JSON.stringify(cookie[0]));
-			if (cookie.length > 0) {
-				chrome.pageAction.show(sender.tab.id);
-	  		}
-		});
+        if (sender.tab.url.indexOf('blacktaboid') > -1 && sender.tab.url.indexOf('servlet/servlet.su') == -1) {
+            chrome.pageAction.setIcon({'tabId':sender.tab.id, 'path':'workbench-3-black-cube-48x48.png'});
+        }
+         // Check if there is a SID related to the page the content script ran on
+        chrome.cookies.getAll({"url":sender.tab.url,"name":"sid"},function (cookie){
+            console.log (cookie.length);
+            console.log(JSON.stringify(cookie[0]));
+            if (cookie.length > 0) {
+                chrome.pageAction.show(sender.tab.id);
+            }
+        });
   
       // Return nothing to let the connection be cleaned up.
       sendResponse({});
@@ -43,17 +43,31 @@ var workbenchtoolsforchrome = {
           return;        
         }
         hostname = tabInfo.hostname;
-	// Get the SID realted to the page the content script ran on
-		chrome.cookies.getAll({"url":tabInfo.url,"name":"sid"},function (cookie){
+        // Get the SID related to the page the content script ran on
+        chrome.cookies.getAll({"url":tabInfo.url,"name":"sid"},function (cookie){
             console.log(JSON.stringify(cookie[0]));
             var sessionId = cookie[0].value; 
-            workbenchtoolsforchrome.loginFromSession(tabInfo.url, sessionId);
-		});
-	  });
+            var domain = cookie[0].domain
+            // If the domain is .salesforce.com, pass to workbench
+            if(domain.indexOf('.salesforce.com') > -1) {
+                workbenchtoolsforchrome.loginFromSession("https://"+ domain, sessionId);
+            // Otherwise, use the first 15 chars of the SID to find the salesforce.com SID cookie
+            } else {
+                var orgID = sessionId.substring(0,15);
+                chrome.cookies.getAll({"name":"sid"},function (cookie){
+                for(i=0;i<cookie.length;i++){
+                    sessionId = cookie[i].value; 
+                    domain = cookie[i].domain;
+                    if (sessionId.indexOf(orgID)>-1 && domain.indexOf('.salesforce.com') > -1) {
+                        workbenchtoolsforchrome.loginFromSession("https://"+ domain, sessionId);
+                    }
+                }
+                });
+            }
+        });
+      });
     },
     
-	
-	
     gotoWorkbench : function(urlTail) {
       if (window.localStorage == null) {
         alert("Local storage must be enabled for Workbench Tools");
@@ -92,68 +106,10 @@ var workbenchtoolsforchrome = {
         alert('Cannot login to Workbench from Blacktab unless you are viewing the Session ID of a user who granted login access.');
         return;
       }
-    
-      var currentUrlTailIndex = currentUrl.indexOf('/',8); //ignore slashes in protocol
-      if (currentUrlTailIndex == -1) {
-        alert(this.badPageErrorMsg);
-        return;
-      }
-      var serverUrlPrefix = currentUrl.substring(0, currentUrlTailIndex);
-    
-
-      if(hostname.indexOf('.salesforce.com') > -1) {
       //pass params to Workbench
-        this.gotoWorkbench('/login.php?serverUrlPrefix=' + escape(serverUrlPrefix) + '&sid=' + escape(sessionId));
-      } else {
-        // We are on a Force.com domain, so that we need first to go to the salesforce.com domain to login, because 
-        // the Session ID is not valid on Force.com domain.
-        
-        // We capture whether we are in lightning
-        isInLightning = false;
-        if(hostname.indexOf('.lightning.force.com') > -1) { 
-          isInLightning = true;
-        }
-
-        // We listen when the tab loads 
-        chrome.tabs.onUpdated.addListener(workbenchtoolsforchrome.listenTabLoading);
-        
-        //we execute the lightning switcher to Salesforce Classic
-        chrome.tabs.create({'url': 'https://' + hostname + '/ltng/switcher?destination=classic'}, (tab) => {
-          tabId = tab.id
-        });
-      }
-        
+      this.gotoWorkbench('/login.php?serverUrlPrefix=' + escape(currentUrl) + '&sid=' + escape(sessionId));  
     },
 
-    listenTabLoading(_tabId, changeInfo) {
-      if(tabId === _tabId && changeInfo.status === 'complete') {
-        chrome.tabs.onUpdated.removeListener(workbenchtoolsforchrome.listenTabLoading);
-        
-        // Now that the salesforce.com page is loaded, we can execute the script to open the Workbench
-        chrome.tabs.executeScript(tabId, {file: "postTabInfo.js"}, () => {
-          
-          if(isInLightning) {
-            // We listen when the tab loads 
-            chrome.tabs.onUpdated.addListener(workbenchtoolsforchrome.listenTabLoading2);
-      
-            // We have to revert back to Lightning, to not change the context of the User
-            chrome.tabs.update(tabId, {url: 'https://' + hostname + '/ltng/switcher?destination=lex'});
-          } else {
-            // We can remove safely the temporary .salesforce.com page
-            chrome.tabs.remove(tabId);
-          }
-        });
-      }
-    },
-
-    listenTabLoading2(_tabId, changeInfo) {
-      if(tabId === _tabId && changeInfo.url.indexOf('.lightning.force.com') > -1) {
-        chrome.tabs.onUpdated.removeListener(workbenchtoolsforchrome.listenTabLoading2);
-
-        //now we are back in Lightning, we can remove safely the temporary .salesforce.com page
-        chrome.tabs.remove(tabId);
-      }
-    }
   };
 
   // REGISTER LISTENERS
